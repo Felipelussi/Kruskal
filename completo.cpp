f138baa18e7d3744ed6c31897a5ff2f91aaa8998
felipe lussi <felipe-p-l@hotmail.com>
2024-12-01 20:18:27 -0300

    Algoritmo traduzido

--- completo.cpp
+++ completo.cpp
@@ -8,142 +8,140 @@ using namespace std;
 class UnionFind
 {
 public: //Especificador de acesso
-    vector<int> elements;
+    vector<int> elementos;
 
     //Construtor
     UnionFind(int n)
     {
-        elements.resize(n);
+        elementos.resize(n);
         for (int i = 0; i < n; i++)
-            elements[i] = i;
+            elementos[i] = i;
     }
 
-    bool connected(int p, int q)
+    bool conectados(int v1, int v2)
     {
-        return elements[p] == elements[q];
+        return elementos[v1] == elementos[v2];
     }
 
-    void unir(int p, int q)
+    void unir(int v1, int v2)
     {
-        int inicial = elements[p];
-        for (int i = 0; i < elements.size(); i++)
-            if (elements[i] == inicial)
-                elements[i] = elements[q];
+        int inicial = elementos[v1];
+        for (int i = 0; i < elementos.size(); i++)
+            if (elementos[i] == inicial)
+                elementos[i] = elementos[v2];
     }
 };
 
-class Edge
+class Aresta
 {
 public: //Especificador de acesso
-    int v;
-    int w;
-    int weight;
+    int v1;
+    int v2;
+    int peso;
 
-    Edge(int v, int w, int weight) //Construtor
+    Aresta(int v1, int v2, int peso) //Construtor
     {
-        this->v = v;
-        this->w = w;
-        this->weight = weight;
+        this->v1 = v1;
+        this->v2 = v2;
+        this->peso = peso;
     };
 };
 
 
-struct CompareEdge //Comparador
+struct ComparaArestaPQ //Comparador
 {
-    bool operator()(const Edge& e1, const Edge& e2) const
+    bool operator()(const Aresta& a1, const Aresta& a2) const
     {
-        return e1.weight > e2.weight;
+        return a1.peso > a2.peso;
     }
 };
 
-struct CompareEdgeSet
+struct CompareArestaSet
 {
-    bool operator()(const Edge& e1, const Edge& e2) const
+    bool operator()(const Aresta& a1, const Aresta& a2) const
     {
-        if (e1.weight != e2.weight) return e1.weight < e2.weight; // Compare weights
-        if (e1.v != e2.v) return e1.v < e2.v; // Break ties by src
-        return e1.w < e2.w; // Break further ties by dest
+        if (a1.peso != a2.peso) return a1.peso < a2.peso;
+        if (a1.v1 != a2.v1) return a1.v1 < a2.v1;
+        return a1.v2 < a2.v2;
     }
 };
 
-class EdgeWeightedGraph
+class Grafo
 {
 public:
     int V;
-    std::vector<std::vector<Edge>> adj;
+    std::vector<std::vector<Aresta>> adj;
 
-    EdgeWeightedGraph(int V)
+    Grafo(int V)
     {
         this->V = V;
-        adj = vector<vector<Edge>>(V, vector<Edge>()); //inicializa vetor de vértices que aponta para vetor de arestas
+        adj = vector<vector<Aresta>>(V, vector<Aresta>()); //inicializa vetor de vértices que aponta para vetor de arestas
     }
 
-    void addEdge(Edge e)
+    void addAresta(Aresta e)
     {
-        adj[e.v].push_back(e);
-        adj[e.w].push_back(e);
+        adj[e.v1].push_back(e);
+        adj[e.v2].push_back(e);
     };
 
-    int getNumberOfVertices()
+    int numeroVertices()
     {
-        set<int> vertex;
+        set<int> vertices;
         for (int i = 0; i < V; i++)
             for (auto e : adj[i])
             {
-                vertex.insert(e.v);
-                vertex.insert(e.w);
+                vertices.insert(e.v1);
+                vertices.insert(e.v2);
             }
-        return vertex.size();
+        return vertices.size();
     }
 
 
-    set<Edge, CompareEdgeSet> get_edges() //Retorna conjunto de todas as arestas únicas do grafo
+    set<Aresta, CompareArestaSet> vertices() //Retorna conjunto de todas as arestas únicas do grafo
     {
-        set<Edge, CompareEdgeSet> edges;
+        set<Aresta, CompareArestaSet> arestas;
         for (int i = 0; i < V; i++)
             for (auto e : adj[i])
-                edges.insert(e);
-        return edges;
+                arestas.insert(e);
+        return arestas;
     }
 };
 
 class Kruskal
 {
 public:
-    vector<Edge> arvoreGeradoraMinima;
+    vector<Aresta> arvoreGeradoraMinima;
 
-    Kruskal(EdgeWeightedGraph g)
+    Kruskal(Grafo g)
     {
-        priority_queue<Edge, vector<Edge>, CompareEdge> filaArestas;
+        priority_queue<Aresta, vector<Aresta>, ComparaArestaPQ> filaArestas;
         //Fila de prioridade usada para ordenar o conjunto de arestas pelo seu peso
-        for (auto e : g.get_edges())
+        for (auto e : g.vertices())
         {
             filaArestas.push(e);
         }
 
         UnionFind uf(g.V);
 
-        while (!filaArestas.empty() && arvoreGeradoraMinima.size() < g.getNumberOfVertices() - 1)
+        while (!filaArestas.empty() && arvoreGeradoraMinima.size() < g.numeroVertices() - 1)
         {
-            Edge e = filaArestas.top();
-            int v = e.v;
-            int w = e.w;
-            if (!uf.connected(v, w))
+            Aresta a = filaArestas.top();
+            if (!uf.conectados(a.v1, a.v2))
             {
-                uf.unir(v, w);
-                arvoreGeradoraMinima.push_back(e);
+                uf.unir(a.v1, a.v2);
+                arvoreGeradoraMinima.push_back(a);
             }
             filaArestas.pop();
         }
     }
 
-    void showMst()
+    void mostraArvoreGeradoraMinima()
     {
         for (int i = 0; i < arvoreGeradoraMinima.size(); i++)
         {
-            int v = arvoreGeradoraMinima[i].v;
-            int w = arvoreGeradoraMinima[i].w;
-            cout << "Aresta: {" << v << ", " << w << "} Peso: " << arvoreGeradoraMinima[i].weight << endl;
+            int v1 = arvoreGeradoraMinima[i].v1;
+            int v2 = arvoreGeradoraMinima[i].v2;
+            cout << "Aresta: {" << v1 << ", " << v2 << "} Peso: " << arvoreGeradoraMinima[i].peso << endl;
         }
     }
 };
@@ -151,36 +149,34 @@ public:
 
 int main()
 {
-    EdgeWeightedGraph e(17);
+    Grafo g(17);
 
-    e.addEdge(Edge(1, 2, 53));
-    e.addEdge(Edge(1, 6, 97));
-    e.addEdge(Edge(1, 8, 32));
-    e.addEdge(Edge(1, 13, 80));
-    e.addEdge(Edge(1, 9, 47));
-    e.addEdge(Edge(1, 7, 76));
-    e.addEdge(Edge(1, 3, 82));
-    e.addEdge(Edge(2, 4, 30));
-    e.addEdge(Edge(3, 4, 80));
-    e.addEdge(Edge(3, 5, 90));
-    e.addEdge(Edge(4, 6, 48));
-    e.addEdge(Edge(5, 7, 83));
-    e.addEdge(Edge(6, 10, 90));
-    e.addEdge(Edge(6, 12, 78));
-    e.addEdge(Edge(7, 9, 46));
-    e.addEdge(Edge(8, 10, 51));
-    e.addEdge(Edge(9, 11, 23));
-    e.addEdge(Edge(10, 14, 40));
-    e.addEdge(Edge(10, 12, 152));
-    e.addEdge(Edge(10, 16, 22));
-    e.addEdge(Edge(11, 13, 33));
-    e.addEdge(Edge(12, 14, 96));
-    e.addEdge(Edge(13, 15, 41));
-    e.addEdge(Edge(14, 16, 38));
-    e.addEdge(Edge(15, 16, 80));
+    g.addAresta(Aresta(1, 2, 53));
+    g.addAresta(Aresta(1, 6, 97));
+    g.addAresta(Aresta(1, 8, 32));
+    g.addAresta(Aresta(1, 13, 80));
+    g.addAresta(Aresta(1, 9, 47));
+    g.addAresta(Aresta(1, 7, 76));
+    g.addAresta(Aresta(1, 3, 82));
+    g.addAresta(Aresta(2, 4, 30));
+    g.addAresta(Aresta(3, 4, 80));
+    g.addAresta(Aresta(3, 5, 90));
+    g.addAresta(Aresta(4, 6, 48));
+    g.addAresta(Aresta(5, 7, 83));
+    g.addAresta(Aresta(6, 10, 90));
+    g.addAresta(Aresta(6, 12, 78));
+    g.addAresta(Aresta(7, 9, 46));
+    g.addAresta(Aresta(8, 10, 51));
+    g.addAresta(Aresta(9, 11, 23));
+    g.addAresta(Aresta(10, 14, 40));
+    g.addAresta(Aresta(10, 12, 152));
+    g.addAresta(Aresta(10, 16, 22));
+    g.addAresta(Aresta(11, 13, 33));
+    g.addAresta(Aresta(12, 14, 96));
+    g.addAresta(Aresta(13, 15, 41));
+    g.addAresta(Aresta(14, 16, 38));
+    g.addAresta(Aresta(15, 16, 80));
 
-
-
-    Kruskal kruskal = Kruskal(e);
-    kruskal.showMst();
+    Kruskal kruskal = Kruskal(g);
+    kruskal.mostraArvoreGeradoraMinima();
 }
